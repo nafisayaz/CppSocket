b5fc7a41809cf22fef1edd638bf5b616fd8d60d5
Sir-Ayaz <sir.ayaz@boostinorbit.com>
2024-03-27 04:53:40 +0530

    Made few changes

--- core/sock/src/serv-sock_t.cpp
+++ core/sock/src/serv-sock_t.cpp
@@ -1,6 +1,5 @@
-
-#include "../headers/serv-sock_t.hpp"
-#include "../../utilities/headers/quick.hpp"
+#include <serv-sock_t.hpp>
+#include <quick.hpp>
 
 
 auto  Servsock_t::get_port()->int const{
@@ -30,22 +29,20 @@ auto Servsock_t::init_with(int port)->bool{
 }
 
 auto  Servsock_t::bind_t()->bool{
-  inet_pton(AF_INET, m_ipAddress, &m_sock_addr.sin_addr );
 
-  if (setsockopt(m_socket, SOL_SOCKET, SO_REUSEADDR, &m_opt, sizeof(m_opt)) < 0){
-    std::cout << "Failed to set SO_REUSEADDR option. "<< "\n";
-    return false;
-  }
+  inet_pton(AF_INET, m_ipAddress, &m_sock_addr.sin_addr );
+  assert( 
+     ( (setsockopt(m_socket, SOL_SOCKET, SO_REUSEADDR, &m_opt, sizeof(m_opt)) == -1 ) , "Failed to set SO_REUSEADDR option." )
+    );
 
   while( bind(m_socket, reinterpret_cast<sockaddr*>(&m_sock_addr), sizeof(m_sock_addr)) == -1){
-      std::cout<< "m_socket binding failed on port number:  " << m_port <<std::endl;
       m_sock_addr.sin_port = htons(m_port=gen_port());
-      // sleep(1);
   }
   
-  if( listen(m_socket, 5) == -1){
-		std::cout<<"listen() failed => "<<strerror(errno)<< "\n";
-	}
+  assert( 
+     ( (listen(m_socket, 5) == -1 ) , strerror(errno) )
+    );
+  
 
   return true;
 
@@ -58,20 +55,16 @@ void Servsock_t::receiver(){
 	  while(true){
 	      int chatfd = accept(m_socket, reinterpret_cast<sockaddr*>(&m_their_addr), &m_their_size);
 
-			  if(recv(chatfd, message, sizeof message, 0) == -1 ){
-			  	std::cout<<"recv() failed !! => "<<strerror(errno)<< "\n";
-        }
-			  else{
-			  	std::cout<<green<<message<<reset<<std::endl;
-          if( send(chatfd, message,  sizeof(message), 0) == -1 ){
-  	      	std::cout<<"send() failed => "<<strerror(errno)<< "\n";
-  	      }else{
-  	      	std::cout<<"Succesfully send from Server => "<<strerror(errno)<< "\n";
-          }
-        }
+        assert( 
+            ( (recv(chatfd, message, sizeof message, 0) == -1) , strerror(errno) )
+          );
+        
+        assert( 
+            ( (send(chatfd, message,  sizeof(message), 0) == -1) , strerror(errno) )
+          );
+
         close(chatfd);  
 			  bzero(message,1024);
-        // sleep(1);
 	}
 
 }
